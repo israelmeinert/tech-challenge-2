51c008d56d685b21d2f1d45402739672c42628de
Paulo Henrique dos Santos <paulo.santos.h@outlook.com>
2025-01-05 17:09:42 -0300

    created edit, delete adjust name of dockerfile add a type for readme.md

--- src/controller/Account.js
+++ src/controller/Account.js
@@ -1,4 +1,4 @@
-const TransactionDTO = require('../models/DetailedAccount')
+const TransactionDTO = require('../models/DetailedAccount');
 
 
 class AccountController {
@@ -17,18 +17,21 @@ class AccountController {
       saveTransaction: require('../feature/Transaction/saveTransaction'),
       getTransaction: require('../feature/Transaction/getTransaction'),
       getCard: require('../feature/Card/getCard'),
-    }, di)
+    }, di);
   }
 
   async find(req, res) {
-    const { accountRepository, getAccount, getCard, getTransaction, transactionRepository, cardRepository } = this.di
+    const { accountRepository, getAccount, getCard, getTransaction, transactionRepository, cardRepository } = this.di;
 
     try {
-      const userId =   req.user.id
-      const account = await getAccount({ repository: accountRepository,  userId })
-      const transactions = await getTransaction({ filter: { accountId: account[0].id }, repository: transactionRepository })
-      const cards = await getCard({ filter: { accountId: account[0].id }, repository: cardRepository })
-    
+      const userId = req.user.id;
+      const account = await getAccount({ repository: accountRepository, userId });
+      const transactions = await getTransaction({
+        filter: { accountId: account[0].id },
+        repository: transactionRepository
+      });
+      const cards = await getCard({ filter: { accountId: account[0].id }, repository: cardRepository });
+
       res.status(200).json({
         message: 'Conta encontrada carregado com sucesso',
         result: {
@@ -36,41 +39,94 @@ class AccountController {
           transactions,
           cards,
         }
-      })
+      });
     } catch (error) {
       res.status(500).json({
         message: 'Erro no servidor'
-      })
+      });
     }
-    
+
   }
 
   async createTransaction(req, res) {
-    const { saveTransaction, transactionRepository } = this.di
-    const { accountId, value, type, from, to } = req.body
-    const transactionDTO = new TransactionDTO({ accountId, value, from, to,  type, date: new Date() })
+    const { saveTransaction, transactionRepository } = this.di;
+    const { accountId, value, type, from, to } = req.body;
+    const transactionDTO = new TransactionDTO({ accountId, value, from, to, type, date: new Date() });
 
-    const transaction = await saveTransaction({ transaction: transactionDTO, repository: transactionRepository })
-    
+    const transaction = await saveTransaction({ transaction: transactionDTO, repository: transactionRepository });
+
     res.status(201).json({
       message: 'Transação criada com sucesso',
       result: transaction
-    })
+    });
   }
 
+  async editTransaction(req, res) {
+    const { transactionRepository } = this.di;
+    const { transactionId } = req.params;
+    const { value, type, from, to } = req.body;
+
+    try {
+      const updatedTransaction = await transactionRepository.update(transactionId, {
+        value,
+        type,
+        from,
+        to,
+      });
+
+      if (!updatedTransaction) {
+        return res.status(404).json({ message: 'Transação não encontrada' });
+      }
+
+      res.status(200).json({
+        message: 'Transação editada com sucesso',
+        result: updatedTransaction,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Erro ao editar a transação',
+        error: error.message,
+      });
+    }
+  }
+
+
+  async deleteTransaction(req, res) {
+    const { transactionRepository } = this.di;
+    const { transactionId } = req.params;
+
+    try {
+      const deletedTransaction = await transactionRepository.deleteById(transactionId);
+
+      if (!deletedTransaction) {
+        return res.status(404).json({ message: 'Transação não encontrada' });
+      }
+
+      res.status(200).json({
+        message: 'Transação excluída com sucesso',
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Erro ao excluir a transação',
+        error: error.message,
+      });
+    }
+  }
+
+
   async getStatment(req, res) {
-    const { getTransaction, transactionRepository } = this.di
+    const { getTransaction, transactionRepository } = this.di;
 
-    const { accountId } = req.params
+    const { accountId } = req.params;
 
-    const transactions = await getTransaction({ filter: { accountId } ,  repository: transactionRepository})
+    const transactions = await getTransaction({ filter: { accountId }, repository: transactionRepository });
     res.status(201).json({
       message: 'Transação criada com sucesso',
       result: {
         transactions
       }
-    })
+    });
   }
 }
 
-module.exports = AccountController
+module.exports = AccountController;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-const { Router } = require('express')
-const AccountController = require('./controller/Account')
-const accountController = new AccountController({})
-const router = Router()
+const { Router } = require('express');
+const AccountController = require('./controller/Account');
+const accountController = new AccountController({});
+const router = Router();
 
 /**
  * @swagger
@@ -15,7 +15,8 @@ const router = Router()
  *       200:
  *         description: Lista de contas encontradas
  */
-router.get('/account', accountController.find.bind(accountController))
+router.get('/account', accountController.find.bind(accountController));
+
 
 /**
  * @swagger
@@ -25,11 +26,102 @@ router.get('/account', accountController.find.bind(accountController))
  *     tags: [Transações]
  *     security:
  *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               accountId:
+ *                 type: string
+ *                 description: ID da conta associada
+ *               value:
+ *                 type: number
+ *                 description: Valor da transação
+ *               type:
+ *                 type: string
+ *                 description: Tipo da transação (e.g., débito ou crédito)
+ *               from:
+ *                 type: string
+ *                 description: Origem da transação
+ *               to:
+ *                 type: string
+ *                 description: Destino da transação
  *     responses:
  *       201:
  *         description: Transação criada com sucesso
  */
-router.post('/account/transaction', accountController.createTransaction.bind(accountController))
+router.post('/account/transaction', accountController.createTransaction.bind(accountController));
+
+/**
+ * @swagger
+ * /account/transaction/{transactionId}:
+ *   put:
+ *     summary: Edita uma transação existente
+ *     tags: [Transações]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: transactionId
+ *         required: true
+ *         description: ID da transação a ser editada
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               value:
+ *                 type: number
+ *                 description: Novo valor da transação
+ *               type:
+ *                 type: string
+ *                 description: Novo tipo da transação
+ *               from:
+ *                 type: string
+ *                 description: Nova origem da transação
+ *               to:
+ *                 type: string
+ *                 description: Novo destino da transação
+ *     responses:
+ *       200:
+ *         description: Transação editada com sucesso
+ *       404:
+ *         description: Transação não encontrada
+ *       500:
+ *         description: Erro ao editar a transação
+ */
+router.put('/account/transaction/:transactionId', accountController.editTransaction.bind(accountController));
+
+/**
+ * @swagger
+ * /account/transaction/{transactionId}:
+ *   delete:
+ *     summary: Exclui uma transação
+ *     tags: [Transações]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: transactionId
+ *         required: true
+ *         description: ID da transação a ser excluída
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Transação excluída com sucesso
+ *       404:
+ *         description: Transação não encontrada
+ *       500:
+ *         description: Erro ao excluir a transação
+ */
+router.delete('/account/transaction/:transactionId', accountController.deleteTransaction.bind(accountController));
 
 /**
  * @swagger
@@ -50,8 +142,8 @@ router.post('/account/transaction', accountController.createTransaction.bind(acc
  *       200:
  *         description: Extrato encontrado
  *       401:
- *         description: Token invalido
+ *         description: Token inválido
  */
-router.get('/account/:accountId/statement', accountController.getStatment.bind(accountController))
+router.get('/account/:accountId/statement', accountController.getStatment.bind(accountController));
 
-module.exports = router
+module.exports = router;
